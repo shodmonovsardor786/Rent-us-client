e1b1487a85d2f84dd804eed8fa6a531502eb418a
Sardor <shodmovsardor786@yandex.ru>
2020-12-19 11:28:57 +0500

    update

--- src/Components/Context.jsx
+++ src/Components/Context.jsx
@@ -4,7 +4,7 @@ const FilterContext = createContext()
 
 //                 port ozgarmidi 4000
 // const ADDRESS = 'https://rent-house-server.herokuapp.com/'
-const ADDRESS = 'https://192.168.1.149:4000'
+const ADDRESS = 'https://localhost:4000'
 
 const FilterProvider = ({children}) => {
 
